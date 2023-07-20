98d9fbd0f3c6ebd5de84e79c24068eb8aa44d16a
Koshal07 <koshalsingh1022@gmail.com>
2023-07-20 11:08:08 +0530

    dsdfgvb

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -14,6 +14,7 @@ const About = ()=>{
         try{
             const res = await fetch('https://mock-exam-portalll-backend.onrender.com/about',{
                 method:'GET',
+                body: JSON.stringify(res),
                 mode: 'cors',
                 headers:{
                     Accept:'application/json',
